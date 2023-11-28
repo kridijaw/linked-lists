6bcd2eac3c615cf8d549069b9f0bbce66710ede7
kurnik <c.jaworski@fu-berlin.de>
2023-11-28 03:03:28 +0100

    initial  commit

---
+++ lib/display.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Error messages
+module Display
+  def error_empty_list
+    'Error: Empty list'
+  end
+
+  def error_index_out_of_range
+    'Error: Index out of range'
+  end
+
+  def error_node_not_in_list
+    'Error: Node not in list'
+  end
+end
