713459b8afd921f40e1c8c8248ec8fa06a7d00d1
kurnik <c.jaworski@fu-berlin.de>
2023-12-01 01:26:49 +0100

    docs: :memo: add YARD doc for class Node

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,17 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
-# Holds a single element of data and pointer to the next node in the list.
+# Holds a single element of data and
+# pointer to the next node in the list.
+#
+# @attr_accessor value [Object] the value of the node
+# @attr_accessor next_node [Node] the next node in the list
 class Node
   attr_accessor :value, :next_node
 
+  # Creates a new node with given value and next_node.
+  #
+  # @param value [Object] the value of the node
+  # @param next_node [Node] the next node in the list
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
