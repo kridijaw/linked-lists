06b7308b57f9244b4691cbfcd8ae756693562a81
kurnik <c.jaworski@fu-berlin.de>
2023-12-01 01:25:36 +0100

    feat: :sparkles: error messages abort now

--- lib/display.rb
+++ lib/display.rb
@@ -3,14 +3,14 @@
 # Error messages
 module Display
   def error_empty_list
-    'Error: Empty list'
+    abort 'Error: Empty list'
   end
 
   def error_index_out_of_range
-    'Error: Index out of range'
+    abort 'Error: Index out of range'
   end
 
   def error_node_not_in_list
-    'Error: Node not in list'
+    abort 'Error: Node not in list'
   end
 end
